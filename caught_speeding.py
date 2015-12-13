4be2fa6070d9609ba06968f1cd82b94e9601bc20
Shubhamoy <me@shubhamoy.com>
2015-12-13 23:29:52 +0530

    Create caught_speeding.py

---
+++ caught_speeding.py
@@ -0,0 +1,13 @@
+def caught_speeding(speed, is_birthday):
+	if is_birthday:
+		if speed <= 65:
+			return 0
+		if speed <= 85:
+			return 1
+		return 2
+	else:
+		if speed <= 60:
+			return 0
+		if speed > 60 and speed <= 80:
+			return 1
+		return 2
